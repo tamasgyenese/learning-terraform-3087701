f90d7381e31147773bd4fb3220d5fad9421ffa31
tamasgyenese <50274324+tamasgyenese@users.noreply.github.com>
2024-02-19 19:26:56 +0100

    use module

--- main.tf
+++ main.tf
@@ -45,6 +45,21 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "new_blog"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+}
+
 resource "aws_security_group_rule" "blog_everyting_out" {
   type        = "egress"
   from_port   = 0
@@ -58,8 +73,8 @@ resource "aws_security_group_rule" "blog_everyting_out" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  
-  vpc_security_group_ids = [aws_security_group.blog.id]
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
